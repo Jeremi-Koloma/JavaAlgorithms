16a920e3c268aef26bd7cd9a61c61862c8d84280
Jeremi-Koloma <kolomajeremi60@gmail.com>
2023-12-16 00:40:37 +0000

    linearSearch with stream

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import java.util.Arrays;
+import java.util.OptionalInt;
+
 public class Main {
 
-    // Function de recherche
+    // Function de recherche linear
     public static Integer linearSearch(int [] array, int item) {
         // Loop
         for (int currentItem : array) {
@@ -12,9 +15,16 @@ public class Main {
     }
 
 
+    // Function de recherche linear en Stream
+    public static OptionalInt linearSearch2(int [] arr, int item) {
+        return Arrays.stream(arr).filter(x -> x== item).findFirst();
+    }
+
+
 
     public static void main(String[] args) {
         int[] array = {1,2,3,5};
         System.out.println(linearSearch(array, 8));
+        linearSearch2(array, 3).ifPresent(System.out::println);
     }
 }
